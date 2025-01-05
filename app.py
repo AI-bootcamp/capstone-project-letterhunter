248ae01c0f6d41cdee30b442fd47d475903aa3f4
mdsri <mdssri0@gmail.com>
2025-01-05 19:39:18 +0300

    merging model with flask

--- app.py
+++ app.py
@@ -1,41 +1,64 @@
+import pandas as pd
 from flask import Flask, render_template, Response
 from ultralytics import YOLO
 import cv2
+import random
 
 app = Flask(__name__)
 
-# تحميل نموذج YOLO
+# Load Vocab.xlsx and prepare data
+vocab_df = pd.read_excel('Vocab.xlsx')
+vocab_dict = dict(zip(vocab_df['Arabic Letter'], vocab_df['Objects in English']))
+
+# YOLO model
 model = YOLO('yolov8n-oiv7.pt')
 
-# تهيئة كاميرا الويب
+# Web Camera
 camera = cv2.VideoCapture(0)
-
 if not camera.isOpened():
-    raise RuntimeError('خطأ: لا يمكن الوصول إلى الكاميرا.')
+    raise RuntimeError('Error: Unable to access the camera.')
+
+# Global variables for the game
+selected_letter = None
+target_classes = []
+found = False
+
+def select_random_letter():
+    global selected_letter, target_classes
+    selected_letter = random.choice(list(vocab_dict.keys()))
+    target_classes = vocab_dict[selected_letter].split(",")  # Assuming objects are comma-separated
 
 def generate_frames():
-    frame_skip = 3
-    frame_count = 0
+    global found
     while True:
         success, frame = camera.read()
-        if not success:
+        if not success or found:
             break
-        else:
-            frame_count += 1
-            if frame_count % frame_skip == 0:
-                # إجراء الكشف باستخدام YOLO
-                results = model.predict(source=frame, verbose=False)
-                annotated_frame = results[0].plot()
+        
+        # YOLO Detection
+        results = model.predict(source=frame, verbose=False)
+        annotated_frame = results[0].plot()
+        
+        # Check for target classes
+        for result in results:
+            for box in result.boxes:
+                class_id = int(box.cls)
+                class_name = result.names[class_id]
+                if class_name in target_classes:
+                    found = True
+                    break
+            if found:
+                break
+        
+        # Show "Correct!!" if found
+        if found:
+            cv2.putText(annotated_frame, "Correct!!", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
 
-                # ترميز الإطار بتنسيق JPEG
-                ret, buffer = cv2.imencode('.jpg', annotated_frame)
-                frame = buffer.tobytes()
-
-                # بث الإطار
-                yield (b'--frame\r\n'
-                       b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-            else:
-                continue
+        # Encode frame
+        _, buffer = cv2.imencode('.jpg', annotated_frame)
+        frame = buffer.tobytes()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 @app.route('/')
 def index():
@@ -43,7 +66,8 @@ def index():
 
 @app.route('/Game')
 def game():
-    return render_template('Game.html')
+    select_random_letter()  # Pick a new letter and targets
+    return render_template('Game.html', letter=selected_letter)
 
 @app.route('/video_feed')
 def video_feed():
